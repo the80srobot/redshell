0059a2ae2a4889d5d84e42c33eac8fdc9b63713a
Adam Sindelar <ats@meta.com>
2024-05-20 08:30:27 +0200

    Git helpers

--- src/git.bash
+++ src/git.bash
@@ -8,4 +8,14 @@ function mkproject() {
     gh repo create "${1}" --private --add-readme --clone
 }
 
+function git-ssh-init() {
+    local remote="$1"
+    local path="$2"
+    local ssh="${GIT_SSH_COMMAND}"
+    [[ -z "${ssh}" ]] && ssh="ssh"
+    "${ssh}" "${remote}" "mkdir -p ${path} && cd ${path} && git init --bare"
+    git remote add origin "${remote}:${path}"
+    git push origin master
+}
+
 fi # _REDSHELL_GIT
