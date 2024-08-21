cda37f09ede4ab456cc187c1dfc3f388f5efe7f7
Adam Sindelar <ats@meta.com>
2024-08-21 10:48:52 +0200

    Fix usage

--- src/path.bash
+++ src/path.bash
@@ -47,7 +47,7 @@ function path_expand() {
   printf '%s\n' "${result%:}"
 }
 
-# Usage: path_push PATH
+# Usage: path_push DIRECTORY
 function path_push() {
   local path="$(path_expand "${1}")"
   pushd "${1}" >/dev/null || return 1
