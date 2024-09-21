fa32f846a431665e03b69d4decb8e85c5d8012cb
Adam Sindelar <ats@meta.com>
2024-09-21 15:22:16 +0200

    hg info

--- src/hg.bash
+++ src/hg.bash
@@ -6,8 +6,11 @@
 if [[ -z "${_REDSHELL_HG}" || -n "${_REDSHELL_RELOAD}" ]]; then
 _REDSHELL_HG=1
 
-# Is the current directory a mercurial repo? Fast check. Prints the path to the
-# repo root, or nothing.
+# Fast check for mercurial. (About 100 times faster than `hg root`.) Prints the
+# root directory of the repository if the current directory is in a repository.
+# Otherwise returns 1.
+#
+# Usage: hg_root
 function hg_root() {
     local root="$(pwd -P)"
     while [[ "${root}" && ! -d "${root}/.hg" ]]
@@ -20,5 +23,34 @@ function hg_root() {
     fi
     echo "${root}"
 }
+
+function hg_repo_name() {
+    local root
+    root="$(hg_root)" || return 1
+    cat "${root}/.hg/reponame" || basename "${root}"
+}
+
+function hg_branch_name() {
+    local root
+    root="$(hg_root)" || return 1
+    cat "${root}/.hg/branch" || hg branch
+}
+
+function hg_ps1_widget() {
+    local root
+    root="$(hg_root)" || return
+    local branch
+    branch="$(hg_branch_name)" || return
+    local changes
+    changes="$(hg status -mard 2> /dev/null | wc -l | tr -d ' ')"
+    local hash
+    hash="$(hg id -i 2> /dev/null)"
+    echo -n "(hg ${branch}:${hash}"
+    if [[ "${changes}" -ne "0" ]]; then
+        echo -n "+${changes}"
+    fi
+    echo -n ") "
+}
+
 
 fi # _REDSHELL_HG
