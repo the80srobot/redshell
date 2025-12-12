82e913d9845b273a09b7034472866e57bc8b39fd
Adam Sindelar <ats@meta.com>
2025-12-12 10:34:50 +0100

    Add pid resume

--- util/pid_suspend/pid_suspend.c
+++ util/pid_suspend/pid_suspend.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
-/* Suppress deprecation warning - syscall() is the only way to call pid_suspend */
+/* Suppress deprecation warning - syscall() is the only way to call pid_suspend/resume */
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <pid>\n", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <suspend|resume> <pid>\n", argv[0]);
         return 1;
     }
 
-    pid_t pid = (pid_t)atoi(argv[1]);
+    const char *action = argv[1];
+    pid_t pid = (pid_t)atoi(argv[2]);
     if (pid <= 0) {
-        fprintf(stderr, "Invalid PID: %s\n", argv[1]);
+        fprintf(stderr, "Invalid PID: %s\n", argv[2]);
         return 1;
     }
 
-    int result = syscall(SYS_pid_suspend, pid);
-    if (result == -1) {
-        perror("pid_suspend failed");
+    int result;
+    if (strcmp(action, "suspend") == 0) {
+        result = syscall(SYS_pid_suspend, pid);
+        if (result == -1) {
+            perror("pid_suspend failed");
+            return 1;
+        }
+        printf("Successfully suspended process %d\n", pid);
+    } else if (strcmp(action, "resume") == 0) {
+        result = syscall(SYS_pid_resume, pid);
+        if (result == -1) {
+            perror("pid_resume failed");
+            return 1;
+        }
+        printf("Successfully resumed process %d\n", pid);
+    } else {
+        fprintf(stderr, "Unknown action: %s (use 'suspend' or 'resume')\n", action);
         return 1;
     }
 
-    printf("Successfully suspended process %d\n", pid);
     return 0;
 }
 
