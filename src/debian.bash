5225f3bac8c259d1a51a34e2869e56b9522c3cbd
Adam Sindelar <adam.sindelar@gmail.com>
2025-12-14 21:49:07 +0100

    mc installer

--- src/debian.bash
+++ src/debian.bash
@@ -38,4 +38,18 @@ function debian_install_or_skip() {
     done
 }
 
+function debian_install_imgcat() {
+    curl -L https://iterm2.com/utilities/imgcat -o ~/bin/imgcat
+    chmod +x ~/bin/imgcat
+}
+
+function debian_setup_mc() {
+    sudo apt-get -y install mc
+    debian_install_imgcat
+    local ini_path="${HOME}/.config/mc/mc.ext.ini"
+    mkdir -p "$(dirname "${ini_path}")"
+    touch "${ini_path}"
+    reinstall_file "${REDSHELL_ROOT}/rc/mc.ext.ini" "${ini_path}"
+}
+
 fi # _REDSHELL_DEBIAN
